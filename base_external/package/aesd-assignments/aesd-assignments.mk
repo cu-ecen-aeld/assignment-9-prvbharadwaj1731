3d2233f7413d8dabf1c2f52bfef68c81ce2498ac
prvbharadwaj1731 <prbh1731@colorado.edu>
2022-10-02 23:08:32 -0400

    Assignment5-part2-latest partially working

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_VERSION = '02a1aea03ffcd5aa430319ab0bbbee4bb80ea36f'
+AESD_ASSIGNMENTS_VERSION = 'a78c22d7590f19327b49ecfd221c93f2cbaf8ba8'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
