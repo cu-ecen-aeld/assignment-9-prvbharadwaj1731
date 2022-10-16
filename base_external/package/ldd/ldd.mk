d1f2d54f136c2297e6804cf01c2f8ae453926cfa
prvbharadwaj1731 <prbh1731@colorado.edu>
2022-10-16 14:38:42 -0400

    Assigment-7-part-2 complete

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,34 @@
+#Assignment 7 makefile
+#Author: Pranav Bharadwaj
+LDD_VERSION = '7e63794b13f6b0b3ee2289225753f56b07b655cd'
+
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-prvbharadwaj1731.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULE = YES
+#LDD_MODULE_SUBDIRS = YES
+
+LDD_MODULE_SUBDIRS += misc-modules/
+LDD_MODULE_SUBDIRS += scull/
+
+#make command for ldd
+define LDD_CMDS
+	$(MAKE)
+endef
+
+#Adding module and scull load/unload utilites
+define LDD_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	
+#	$(INSTALL) -m 0755 $(TARGET_DIR)/lib/modules/5.15.18/extra/
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/lib/modules/5.15.18/extra/hello.ko
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/faulty.ko $(TARGET_DIR)/lib/modules/5.15.18/extra/faulty.ko
+#	$(INSTALL) -m 0755 $(@D)/scull/scull.ko $(TARGET_DIR)/lib/modules/5.15.18/extra/scull.ko
+	
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
